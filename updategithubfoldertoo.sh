975a3473819d40ac5276b365548b351a46f8dc52
NamekianUnix <41169686+NamekianUnix@users.noreply.github.com>
2019-03-24 15:09:58 -0400

    Add files via upload

--- updategithubfoldertoo.sh
+++ updategithubfoldertoo.sh
@@ -16,10 +16,12 @@ apt full-upgrade -y
 apt autoremove -y
 apt autoclean
 apt clean
+mv /root/Kali-Linux-Bash-Scripts/updategithubfoldertoo.sh /root/updategithubfoldertoo.sh
 echo "Out with the old"
 rm -r /root/Kali-Linux-Bash-Scripts/
 echo "In with the new"
 git clone https://github.com/NamekianUnix/Kali-Linux-Bash-Scripts.git
 cd /root/Kali-Linux-Bash-Scripts/
 chmod +x *
+rm /root/updategithubfoldertoo.sh
 echo "Done"
