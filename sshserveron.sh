7b58c47b45457439c189edaf6978563be3d608f4
NamekianUnix <41169686+NamekianUnix@users.noreply.github.com>
2019-05-19 14:34:40 -0400

    Add files via upload

---
+++ sshserveron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: NamekianUnix
+# Last updated: 5/19/19
+# Purpose: starts ssh service
+
+# References:
+# https://dephace.com/how-to-setup-ssh-server-on-kali-linux/
+
+# This script assumes the following:
+# You're using Kali Linux
+# You're logged in as root, or a SU terminal
+# You've setup an shh server securely
+# You have an active internet connection
+
+service ssh start
